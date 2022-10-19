57438126bd66ba35a3582a08fea098eec68907b6
SOOMRAQ <109300209+SOOMRAQ@users.noreply.github.com>
2022-10-19 08:19:30 +0000

    Deploying to gh-pages from @ SOOMRAQ/goit-js-hw-09@f71599fcb28188a31e1f868925481261e9122539 🚀

---
+++ 01-color-switcher.e96443b6.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),r=document.querySelector("body");let a=null;e.addEventListener("click",(t=>{a=setInterval((()=>r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`),1e3),e.disabled=!0})),t.addEventListener("click",(t=>{clearInterval(a),e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.e96443b6.js.map
