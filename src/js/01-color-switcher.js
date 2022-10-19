f71599fcb28188a31e1f868925481261e9122539
Nikita Taranenko <soomraq.x@gmail.com>
2022-10-19 11:18:54 +0300

    1 task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandowHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+// console.log(startBtn);
+
+let timer = null;
+
+startBtn.addEventListener('click', event => {
+  timer = setInterval(
+    () => (body.style.backgroundColor = getRandowHexColor()),
+    1000
+  );
+  startBtn.disabled = true;
+});
+
+stopBtn.addEventListener('click', event => {
+  clearInterval(timer);
+  startBtn.disabled = false;
+});
