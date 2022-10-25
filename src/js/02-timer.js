d98523b77c61223ac21994950b3236a544b39eb2
Nikita Taranenko <soomraq.x@gmail.com>
2022-10-25 22:10:55 +0300

    fix timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,65 +1,33 @@
+// Описаний в документації
 import flatpickr from 'flatpickr';
-import 'flatpickr/dist/themes/dark.css';
-import Notiflix from 'notiflix';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
 
-// error settings
-
-Notiflix.Report.init({
-  className: 'notiflix-report',
-  width: '200px',
-  backgroundColor: '#f8f8f8',
-  borderRadius: '25px',
-  rtl: false,
-  zindex: 4002,
-  backOverlay: false,
-  backOverlayColor: 'rgba(0,0,0,0.5)',
-  backOverlayClickToClose: false,
-  fontFamily: 'Quicksand',
-  svgSize: '60px',
-  plainText: true,
-  titleFontSize: '16px',
-  titleMaxLength: 34,
-  messageFontSize: '13px',
-  messageMaxLength: 400,
-  buttonFontSize: '14px',
-  buttonMaxLength: 34,
-  cssAnimation: true,
-  cssAnimationDuration: 360,
-  cssAnimationStyle: 'fade', // 'fade' - 'zoom'
-
-  failure: {
-    svgColor: '#ff5549',
-    titleColor: '#1e1e1e',
-    messageColor: '#242424',
-    buttonBackground: '#ff5549',
-    buttonColor: '#fff',
-    backOverlayColor: 'rgba(255,85,73,0.2)',
-  },
-});
-
-const startBtn = document.querySelector('[data-start]');
-const calendar = document.querySelector('#datetime-picker');
-// console.log(calendar);
-const timerDays = document.querySelector('[data-days]');
-const timerHours = document.querySelector('[data-hours]');
-const timerMinutes = document.querySelector('[data-minutes]');
-const timerSeconds = document.querySelector('[data-seconds]');
+const startBtn = document.querySelector('button[data-start]');
 startBtn.disabled = true;
+
+const calendar = document.querySelector('#datetime-picker');
+
+const timerValue = {
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+let timerId = null;
+
+/////////////////////////////////////////////////////////
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    if (selectedDates[0].getTime() < Date.now()) {
-      Notiflix.Report.failure(
-        'Error',
-        'Please choose data in the future',
-        'Cancel'
-      );
+    if (selectedDates[0] < new Date()) {
+      startBtn.disabled = true;
+      window.alert('Please choose a date in the future');
     } else {
       startBtn.disabled = false;
       startBtn.addEventListener('click', () => {
@@ -69,26 +37,7 @@ const options = {
   },
 };
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = addLeadingZero(Math.floor(ms / day));
-  // Remaining hours
-  const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  // Remaining minutes
-  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
-  const seconds = addLeadingZero(
-    Math.floor((((ms % day) % hour) % minute) / second)
-  );
-
-  return { days, hours, minutes, seconds };
-}
+flatpickr(calendar, options);
 
 function changeTimer() {
   let timer = setInterval(() => {
@@ -112,4 +61,23 @@ function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
 
-flatpickr(calendar, options);
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
